6a2b3ca936883bc725b07e747d38e290a13fd67f
Bondchen <bonnie.hoika@gmx.de>
2025-03-20 09:54:49 +0100

    modern way: output()

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +11,8 @@ export class UserComponent {
   @Input() id!: string;
   @Input() avatar!: string;
   @Input() name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>(); //hiermit braucht mein nicht mehr speziell sagen, dass es newEventEmitter beinhaltet. Aber Wir müssen jetzt TS sagen, welcher typ die daten sind. Es sind auch keine signals im gegensatz zum input
 
   get imagePath() {
     return '/assets/users/' + this.avatar;
